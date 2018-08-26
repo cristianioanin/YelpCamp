943bb7a31be039c08cf91aaec340ed9ff9a8aee1
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-08-26 22:01:54 +0300

    Creating a new Campground item
    setup POST route for adding a new campground;
    install body-parser;
    create form component;
    setup route to show the form;
    downgraded Bootstrap version to match the course: v3.3.x

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const campgrounds = [
+  {
+    name: 'Salmon Creek',
+    image: 'https://images.pexels.com/photos/221436/pexels-photo-221436.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
+  },
+  {
+    name: 'Mountain Goat\'s Rest',
+    image: 'https://images.pexels.com/photos/344100/pexels-photo-344100.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
+  }
+];
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    {
-      name: 'Salmon Creek',
-      image: 'https://images.pexels.com/photos/221436/pexels-photo-221436.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-    },
-    {
-      name: 'Granite Hill',
-      image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-    },
-    {
-      name: 'Mountain Goat\'s Rest',
-      image: 'https://images.pexels.com/photos/344100/pexels-photo-344100.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-    }
-  ];
-
   res.render('campgrounds', { campgrounds });
 });
 
+app.post('/campgrounds', (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = { name, image };
+
+  campgrounds.push(newCampground);
+
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
+});
 
 
 app.listen(3000, () => console.log('YelpCamp server is running on port 3000'));
