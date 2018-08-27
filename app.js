c2344c3239e2446b7a7f99aff90a3614613b64b7
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-08-27 12:29:45 +0300

    Added data persistence
    installed mongoose and connected the app to a Mongo database; create and read routes connected to db

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image: 'https://images.pexels.com/photos/221436/pexels-photo-221436.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-  },
-  {
-    name: 'Mountain Goat\'s Rest',
-    image: 'https://images.pexels.com/photos/344100/pexels-photo-344100.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-  }
-];
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', { campgrounds });
+  Campground.find({}, (err, campgrounds) => {
+    if (err) console.log('Fail:', err);
+    else res.render('campgrounds', { campgrounds });
+  });
 });
 
 app.post('/campgrounds', (req, res) => {
@@ -33,9 +30,10 @@ app.post('/campgrounds', (req, res) => {
   const image = req.body.image;
   const newCampground = { name, image };
 
-  campgrounds.push(newCampground);
-
-  res.redirect('/campgrounds');
+  Campground.create(newCampground, (err, dbCampground) => {
+    if (err) console.log('Fail:', err);
+    else res.redirect('/campgrounds');
+  });
 });
 
 app.get('/campgrounds/new', (req, res) => {
