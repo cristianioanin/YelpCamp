037fc57fa9dfd270756dd9cf257515e88680711b
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-08-27 14:01:18 +0300

    Added SHOW route
    modified the db schema to take in an object with added description property;
    added show route to display extended information about the items;
    updated the add new campground form of NEW route to accept the description as input;
    updated CREATE route to add objects accordingly to the new db schema

--- app.js
+++ app.js
@@ -9,26 +9,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 const Campground = mongoose.model('Campground', campgroundSchema);
+
+// // FOR TESTING PURPOSES - arbitrarily add data to database
+// Campground.create(
+//   {
+//     name: 'Granite Hill',
+//     image: 'https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+//     description: 'This is a huge granite hill. No bathrooms, no water, just beautiful granite!'
+//   },
+//   (err, campground) => {
+//     if (err) console.log(err);
+//     else console.log(campground);
+//   }
+// );
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
+// INDEX Route
 app.get('/campgrounds', (req, res) => {
   Campground.find({}, (err, campgrounds) => {
     if (err) console.log('Fail:', err);
-    else res.render('campgrounds', { campgrounds });
+    else res.render('index', { campgrounds });
   });
 });
 
+// CREATE Route
 app.post('/campgrounds', (req, res) => {
   const name = req.body.name;
   const image = req.body.image;
-  const newCampground = { name, image };
+  const description = req.body.description;
+  const newCampground = { name, image, description };
 
   Campground.create(newCampground, (err, dbCampground) => {
     if (err) console.log('Fail:', err);
@@ -36,8 +53,17 @@ app.post('/campgrounds', (req, res) => {
   });
 });
 
+// NEW Route
 app.get('/campgrounds/new', (req, res) => {
   res.render('new');
+});
+
+// SHOW Route
+app.get('/campgrounds/:id', (req, res) => {
+  Campground.findById(req.params.id, (err, dbCampground) => {
+    if (err) console.log(err);
+    else res.render('show', { campground: dbCampground });
+  });
 });
 
 
