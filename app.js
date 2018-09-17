9f6f7094b942760c47eff42a3271b3fdf0f67145
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-09-17 21:43:35 +0300

    test mlab connection with direct credentials

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const localStrategy = require('passport-local');
 const methodOverride = require('method-override');
 
 // mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
-mongoose.connect(`mongodb://${process.env.MLAB_DB_USER}:${process.env.MLAB_DB_PASS}@ds161112.mlab.com:61112/yelpcamp`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://sareth:donald22@ds161112.mlab.com:61112/yelpcamp`, { useNewUrlParser: true });
 mongoose.set('useFindAndModify', false);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
